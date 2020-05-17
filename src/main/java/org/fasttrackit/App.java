ca2465722d7b74780d29e217fc09d23243b40c9d
AncutaBiro <ancuta.biro@yahoo.com>
2020-05-17 22:36:09 +0300

    implemented CRUD methods in AgendaRepository

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -16,18 +16,23 @@ public class App
     public static void main( String[] args ) throws IOException, SQLException {
 
 //        CreateAgendaRequest request = new CreateAgendaRequest();
-//        request.setFirstName("Ancuta");
-//        request.setLastName("Biro");
+//        request.setFirstName("Daniel");
+//        request.setLastName("Pop");
 //        request.setPhoneNumber("0740000000");
-//
+
         AgendaRepository repository = new AgendaRepository();
 //        repository.createAgenda(request);
 
-//        UpdateAgendaRequest request1 = new UpdateAgendaRequest();
+        UpdateAgendaRequest request1 = new UpdateAgendaRequest();
 //        request1.setFirstName("Ancuta");
 //        request1.setFirstName("ANCUTA");
 
-        repository.deleteAgenda(1);
+//        repository.deleteAgenda(1);
+
+//        System.out.println(repository.getAgenda());
+
+        request1.setLastName("Pop");
+        System.out.println(repository.getAgendaByLastName(request1));
 
     }
 }
--- src/main/java/org/fasttrackit/persistence/AgendaRepository.java
+++ src/main/java/org/fasttrackit/persistence/AgendaRepository.java
@@ -1,12 +1,12 @@
 package org.fasttrackit.persistence;
 
+import org.fasttrackit.domain.Agenda;
 import org.fasttrackit.transfer.CreateAgendaRequest;
 import org.fasttrackit.transfer.UpdateAgendaRequest;
-
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AgendaRepository {
 
@@ -53,11 +53,57 @@ public class AgendaRepository {
 
     }
 
+    public List<Agenda> getAgenda () throws IOException, SQLException {
 
+        String sql = "SELECT id, first_name, last_name, phone_number FROM agenda";
 
+        List<Agenda> agenda = new ArrayList<>();
 
+        try (Connection connection = DatabaseConfiguration.getConnection();
+        Statement statement = connection.createStatement()) {
 
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                Agenda contacts = new Agenda();
+                contacts.setId(resultSet.getLong("id"));
+                contacts.setFirstName(resultSet.getString("first_name"));
+                contacts.setLastName(resultSet.getString("last_name"));
+                contacts.setPhoneNumber(resultSet.getString("phone_number"));
+
+                agenda.add(contacts);
+            }
+        }
+      return agenda;
     }
+
+    public List<Agenda> getAgendaByLastName (UpdateAgendaRequest lastName) throws IOException, SQLException {
+
+        List<Agenda> agenda = new ArrayList<>();
+
+        String sql = "SELECT id, first_name, last_name, phone_number FROM agenda WHERE last_name = ?";
+
+        try (Connection connection = DatabaseConfiguration.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setString(1, lastName.getLastName());
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Agenda contact = new Agenda();
+                contact.setId(resultSet.getLong("id"));
+                contact.setFirstName(resultSet.getString("first_name"));
+                contact.setLastName(resultSet.getString("last_name"));
+                contact.setPhoneNumber(resultSet.getString("phone_number"));
+
+                agenda.add(contact);
+            }
+        }
+        return agenda;
+    }
+
+}
 
 
 
